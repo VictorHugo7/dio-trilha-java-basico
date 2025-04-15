aecb59a65492ff465d22b0e7959776465f0a2a7f
VictorHugo7 <victorhugoaraujo1999@gmail.com>
2025-04-15 10:08:39 -0300

    Enviando a resolução do desafio

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,39 @@
+package src;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+
+        // Coleta de dados do usuário
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite sua conta: ");
+        String conta = scanner.nextLine();
+
+        System.out.print("Digite seu CPF: ");
+        String cpf = scanner.nextLine();
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        // Exemplo de saldo fixo (poderia vir de outro lugar no futuro)
+        double saldo = 1250.75;
+
+        // Apresentação dos dados
+        System.out.println("\"Olá, " + nome +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+        System.out.println("\n--- Dados da Conta Bancária ---");
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Agência: " + agencia);
+        System.out.println("Conta: " + conta);
+        System.out.printf("Saldo: R$ %.2f\n", saldo);
+
+        scanner.close();
+    }
+}
+
